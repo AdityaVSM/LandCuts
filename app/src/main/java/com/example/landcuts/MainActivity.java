b09d7df5f5dd033838fa59271aa2f6d0fe4c92ce
akash-sangnure12 <akashashok.is19@bmsce.ac.in>
2022-01-15 20:00:21 +0530

    splash anim

--- app/src/main/java/com/example/landcuts/MainActivity.java
+++ app/src/main/java/com/example/landcuts/MainActivity.java
@@ -1,35 +1,32 @@
 package com.example.landcuts;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.text.HtmlCompat;
-import androidx.viewpager2.widget.ViewPager2;
+import android.os.Bundle;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.media.Image;
-import android.os.Bundle;
+
 import android.os.Handler;
-import android.text.Html;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.example.landcuts.Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
     Animation top_anim,bottom_anim;
     TextView txt_anim,txt_anim2;
     ImageView image_anim;
 
+
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      
 
         top_anim = AnimationUtils.loadAnimation(this,R.animator.top_animation);
         bottom_anim = AnimationUtils.loadAnimation(this,R.animator.bottom_animation);
@@ -42,13 +39,10 @@ public class MainActivity extends AppCompatActivity {
         image_anim.setAnimation(top_anim);
         txt_anim2.setAnimation(bottom_anim);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(i);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+            finish();
         },3000);
 
 

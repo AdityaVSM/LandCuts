d8ac87d7ad92a8aa8e9e8d7334f207698529beec
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2022-01-21 15:11:34 +0530

    added logout button

--- app/src/main/java/com/example/landcuts/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/landcuts/Fragments/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.landcuts.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.landcuts.LoginActivity;
 import com.example.landcuts.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment {
+    FirebaseAuth auth;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -19,6 +24,18 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        auth = FirebaseAuth.getInstance();
+        View view =  inflater.inflate(R.layout.fragment_settings, container, false);
+
+        ImageButton logout_button = view.findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+        return view;
     }
 }
